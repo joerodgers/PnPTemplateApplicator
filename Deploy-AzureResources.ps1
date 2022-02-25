da6c37192f1f67fed0e8272b10d423bd4bd72fee
joerodgers <28455042+joerodgers@users.noreply.github.com>
2022-02-25 16:02:49 -0500

    Publishing requirements.psd1 to function app with PnP.PowerShell 1.* configured

--- Deploy-AzureResources.ps1
+++ Deploy-AzureResources.ps1
@@ -14,7 +14,9 @@ $templatePath        = "$PSScriptRoot\function\azure-function-arm-template.json"
 $functionCodePath    = "$PSScriptRoot\function\function.ps1"
 $resourceGroupName   = "RG-SPOTEMPLATEAPPLICATOR-NPROD-USEAST"
 $templateZipPath     = "$PSScriptRoot\function\templates.zip"
+$requirementsZipPath = "$PSScriptRoot\function\requirements.zip"
 $subscriptionId      = "d432671f-fd2d-449f-afdf-010ba093eace"
+
 
 Select-AzSubscription -Subscription $subscriptionId -WarningAction SilentlyContinue
 
@@ -23,7 +25,7 @@ $templateParameters  = @{
     tenantId               = $tenantId
     certificateThumbprint  = $thumbprint
     certificatePfxPassword = $certificatePassword
-    certificatePfxBase64   = [System.Convert]::ToBase64String( (Get-Content -Path $certificatePath -Encoding Byte) )
+    certificatePfxBase64   = [System.Convert]::ToBase64String( (Get-Content -Path $certificatePath -AsByteStream -Raw) )
     functionCode           = (Get-Content -Path $functionCodePath -Raw).ToString()
 }
 
@@ -40,21 +42,19 @@ if( Test-Path -Path $templatePath -PathType Leaf )
 
 if( $deployment -and (Test-Path -Path $templateZipPath -PathType Leaf) )
 {
-    $credentials = Invoke-AzResourceAction `
-                        -ResourceGroupName $resourceGroupName `
-                        -ResourceType      "Microsoft.Web/sites/config" `
-                        -ResourceName      "$($deployment.Outputs.functionAppName.Value)/publishingcredentials" `
-                        -Action            "list" `
-                        -ApiVersion        "2015-08-01" `
-                        -Force
+    if( Get-AzWebApp -ResourceGroupName $resourceGroupName -Name $deployment.Outputs.functionAppName.Value )
+    {
+        $null = Publish-AzWebApp `
+                    -ResourceGroupName $resourceGroupName `
+                    -Name              $deployment.Outputs.functionAppName.Value `
+                    -ArchivePath       $templateZipPath `
+                    -Force
 
-    $base64AuthHeader = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes( ("{0}:{1}" -f $credentials.properties.publishingUserName, $credentials.properties.publishingPassword) ) )
-
-    Invoke-RestMethod `
-        -Uri         "https://$($deployment.Outputs.functionAppName.Value).scm.azurewebsites.net/api/zip/site/wwwroot/$deployment.Outputs.functionName.Value" `
-        -Headers     @{ Authorization = "Basic $base64AuthHeader" } `
-        -Method      PUT `
-        -InFile      $templateZipPath `
-        -ContentType "multipart/form-data"
-
+        $null = Publish-AzWebApp `
+                    -ResourceGroupName $resourceGroupName `
+                    -Name              $deployment.Outputs.functionAppName.Value `
+                    -ArchivePath       $requirementsZipPath `
+                    -Force
+    }
 }
+
